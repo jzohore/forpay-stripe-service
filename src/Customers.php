a90e8161043d706a35c9a25382733f911533d8b9
Junior Zohore <sonny_dev@wifi-sge-7-092.siege.inria.fr>
2024-08-30 15:02:18 +0200

    Préparation de la version 1.0.0

--- src/Customers.php
+++ src/Customers.php
@@ -1,14 +1,23 @@
 <?php
 
 use Stripe\Customer;
+use Stripe\Exception\ApiErrorException;
 
 class Customers extends StripeLogin
 {
     /**
-     * @throws \Stripe\Exception\ApiErrorException
+     * @throws ApiErrorException
      */
     public function createCustomer(array $data): Customer
     {
         return $this->getPrivateKeyStripe()->customers->create($data);
+    }
+
+    /**
+     * @throws ApiErrorException
+     */
+    public function updateCustomer(?string $customerId, array $data): Customer
+    {
+        return $this->getPrivateKeyStripe()->customers->update($customerId, $data);
     }
 }
